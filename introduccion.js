881eb12d25bfe3ac464a654f202986d31b75f236
ivan diaz <owivan.91@gmail.com>
2023-08-18 19:09:01 -0600

    add new class

---
+++ introduccion.js
@@ -0,0 +1,50 @@
+let apellido;
+console.log(apellido);
+
+
+let persona = 122134234;
+let otravariable = 'asdasdas';
+//"alkslkadlkasldkjasd" => 'string'
+//true false => boolean
+const objeto = 'carro';
+objeto = 'perro'
+
+persona = 'freddy';
+console.log(persona)
+console.log(objeto)
+
+//vacio = 'hola mundo'
+const contador = 0;
+// var contador = 0; 
+
+// Operadores
+
+
+// aritmeticos
++ Adicion
+- Sustraccion
+* Multiplicacion
+/ Division
+% Modulo 
+** Exponenciacion
+++ incremento
+-- Decremento
+
+
+console.log(2 + '2')
+const primerNumero = Number('2')
+
+console.log('Hola' + 'Mundo')
+console.log(primerNumero + 2)
+console.log(typeof 2)
+
+let numero = 4
+
+//console.log(++numero);
+console.log(--numero);
+
+// Logicos
+
+
+// 
+
