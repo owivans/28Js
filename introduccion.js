dc2f24f5e3b486885be7e437c5b60b325084e607
ivan diaz <owivan.91@gmail.com>
2023-08-21 18:54:15 -0600

    add new class

--- introduccion.js
+++ introduccion.js
@@ -44,7 +44,117 @@ let numero = 4
 console.log(--numero);
 
 // Logicos
+&& And
+|| Or
+!
+≥
+≤
+==
+=== = = = 
 
+console.log(2 > 3)
+
+const esAdulto = false;
+const negacion = !esAdulto;
+console.log(negacion)
+console.log(2 == '2')
+console.log(2 === '2')
+console.log(1 == true)
+console.log(1 === true)
+
+console.log(0 == false )
+
+falsy
+truthy
 
 // 
+
+# Sesión 1 - Introducción a JS
+
+# Temario
+
+### ¿Qué es JavaScript y para qué sirve?
+
+- De dónde salió JS?
+    - NetScape
+- ¿Para qué  se puede usar?
+    - web
+    - Aplicaciones de red
+    - Aplicaciones móviles
+    - Aplicaciones desktop
+    - IoT
+- ¿JavaScript es lo mismo que Java?
+    - No
+- ¿Por qué es importante JS en el desarrollo web?
+- Instalación y uso de Code Runner
+- Hola mundo
+- Syntaxis
+    - Variables
+        - ¿Qué es una variable?
+            - Restricciones de nombrado
+                - Names can contain letters, digits, underscores, and dollar signs.
+                - Names must begin with a letter
+                - Names can also begin with $ and _ (but we will not use it in this tutorial)
+                - Names are case sensitive (y and Y are different variables)
+                - Reserved words (like JavaScript keywords) cannot be used as names
+            - Mejores practicas de nombrado
+                - Nombres descriptivos
+                - lowerCamelCase
+        - Como declarar una variable
+            - const
+            - let
+            - var
+        - Tipos de datos primitivos
+            - Number
+            - String | String templates
+            - Boolean
+            - null
+            - undefined
+        - typeof
+    - Operadores
+        - Asignación
+            - =
+        - Aritméticos
+            - Adición ( + )
+            - Sustracción ( - )
+            - Multiplicación ( * )
+            - División ( / )
+            - Exponenciación ( ** )
+            - Modulo ( % )
+            - Incremento ( ++ )
+            - Decremento ( — )
+        - Lógicos
+            - &&
+            - ||
+            - ≥
+            - ≤
+            - >
+            - <
+            - ==
+            - ===
+    - Estructuras de control
+        - if
+        - if Else
+        - switch
+        
+
+## Ejercicios propuestos
+
+- Declarar variables para diferentes tipos de datos
+    
+    Declara variables para los siguientes datos
+    
+    - Nombre de tu Ciudad
+    - Año de nacimiento
+    - Nombre propio
+    - Tu estatura
+    - ¿El cielo es azul?
+    - Sentencia “Hola Soy X y vivo en Y” usando variables anteriores
+    - Numero de patas de una mesa
+    - Mes de nacimiento
+    - Nombre de tu serie favorita
+    - El número pi
+- Programa que te dice si un numero es mayor que otro o no
+    1. Se declaran 2 variables tipo numero
+    2. Se declara una variable de tipo booleano para albergar el resultado final
 
---
+++ operadores.js
@@ -0,0 +1,170 @@
+console.log(2 > 1 && 4 > 5 && 1 > 0)
+
+||
+
+or 
+console.log(2 > 1 || 4 > 5 || 1 > 0)
+
+const tengoMonedas = false;
+
+// if( 2 > 1 && 4 > 5 ){
+
+// };
+
+if( tengoMonedas ) {
+    console.log('voy a comprar')
+}
+
+if( !tengoMonedas ) {
+    console.log('consigue monedas')
+}
+
+const estaSol = false;
+
+if(estaSol) {
+    console.log('me pongo lentes de sol')
+} else {
+    console.log('salgo')
+}
+
+
+
+
+
+
+const clima = 'sdfsdf';
+let prenda = 'camisa';
+
+if(clima === 'calor'){
+    prenda = 'short'; 
+} else if (clima === 'frio'){
+    prenda = 'abrigo';
+} else if(clima === 'lluvioso'){
+    prenda = 'impermeable'
+} else {
+    prenda = 'pijama'
+}
+
+console.log(prenda)
+
+
+if(clima === 'calor'){
+    prenda = 'short'; 
+} 
+
+if (clima === 'frio'){
+    prenda = 'abrigo';
+}
+
+if(clima === 'lluvioso'){
+    prenda = 'impermeable'
+}
+
+prenda = 'pijama'
+
+
+const password = 'cacahuate';
+const authFacial = true;
+let usuarioValido = false;
+const email = 'email1@.com';
+const userName = 'mentor91';
+
+const isUserNameValid = email === 'emil1@.com' || userName === 'mentor91';
+
+if(password === 'cacahuate' && authFacial && isUserNameValid ){
+    usuarioValido = true;
+    console.log('usuario es valido')
+} else {
+    console.log('usuario no valido')
+}
+
+console.log(usuarioValido)
+
+
+
+
+
+
+1.Verificar si un número es positivo, negativo o cero.
+
+-declarar 3 variables y almacenar un numero
+- evaluar cada numero si es:
+    positivo: numero mayor 0
+    negativo: numero menor que 0
+-mostramos en la terminal el resultado
+
+const numero = -10;
+
+if(numero === 0){
+    console.log('el numero es cero')
+}else if(numero > 0){
+    console.log('es un numero positivo');
+}else if(numero < 0){
+    console.log('es un numero negativo')
+}else {
+    console.log('el numero que ingresaste no es valido')
+}
+
+
+2.Determinar si un número es par o impar.
+
+-declarar la varible
+-evaluamos si el numero de la variable es un par o impar
+-aplicarle el operador de modulo a la variable
+- si el resultado de la operacion es 0 es un numero par
+- si el resultado de la operacion es 1 es un numero impar
+-mostrarlo en la terminal
+
+const numeroAEvaluar = 10;
+
+if(numeroAEvaluar % 2 === 0){
+    console.log('este numero es par')
+} else{
+    console.log('este numero es impar')
+}
+
+
+console.log(1 != '1')
+console.log(1 !==)
+
+
+3.Calificar un examen en base a la puntuación.
+ejemplo:
+si es mayor o igual a 90 es 'A'
+si es mayor o igual a 80 es 'B'
+si es mayor o igual a 70 es 'C'
+si es mayor o igual a 60 es 'D'
+
+
+switch (key) {
+    case value:
+        
+        break;
+    case value:
+    
+        break;
+
+    case value:
+
+        break;
+
+    default:
+        break;
+}
+
+if(condicion){
+
+}
+
+if(condicion){
+
+}
+
+if(condicion){
+
+}
+
+if(condicion){
+
+}
+
